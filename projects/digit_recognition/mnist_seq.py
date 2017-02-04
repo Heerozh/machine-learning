1d1b723f8879184f1efe8a724aaa89f41cf91acb
heeroz <zjh@xyz-soft.com>
2017-02-05 07:34:42 +0800

    call me 天灾

--- projects/digit_recognition/mnist_seq.py
+++ projects/digit_recognition/mnist_seq.py
@@ -7,24 +7,27 @@ import input_data
 MNIST = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 MNIST_IMG_SZ = 28
-DIGITS = 0x0A
+DIGITS = 0x0A + 1
+
 
 def gen_seq_img(images, labels, seqs=5):
     """generate one seqed img"""
     img = np.ndarray([MNIST_IMG_SZ, 0], dtype=np.float32)
     lab = np.zeros([seqs, DIGITS], dtype=np.float32)
-    rndlen = random.randint(1, seqs)
+    rnd_len = random.randint(1, seqs)
     for i in range(0, seqs):
-        if i > rndlen:
+        if i > rnd_len:
             char = np.zeros_like(images[0]).reshape(MNIST_IMG_SZ, MNIST_IMG_SZ)
+            lab[i, DIGITS - 1] = 1
         else:
-            rndsel = random.randint(0, len(images)-1)
-            char = images[rndsel].reshape(MNIST_IMG_SZ, MNIST_IMG_SZ)
-            lab[i, np.argmax(labels[rndsel])] = 1
+            rnd_sel = random.randint(0, len(images)-1)
+            char = images[rnd_sel].reshape(MNIST_IMG_SZ, MNIST_IMG_SZ)
+            lab[i, np.argmax(labels[rnd_sel])] = 1
         img = np.hstack((img, char))
     return img, lab
 
-def gen_datas(size, test=False, seqs=5):
+
+def gen_data(size, test=False, seqs=5):
     """generate dataset"""
     x_dat = np.ndarray([size, MNIST_IMG_SZ, MNIST_IMG_SZ * seqs, 1], dtype=np.float32)
     y_dat = np.ndarray([size, seqs, DIGITS], dtype=np.float32)
